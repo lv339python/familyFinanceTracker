295870a1037085dd4a80e5e166205d05769d5aa3
antooa <antooa@yandex.ru>
2018-10-06 19:26:30 +0300

    Created home page with router

---
+++ iBudget/static/src/components/SideBar.vue
@@ -0,0 +1,38 @@
+<template>
+       <div id="header">
+      <aside>
+            <div id="buttons">
+               <b-button-group id="navbtns" vertical>
+                 <b-button :variant="secondary" to="/home">HOME</b-button>
+                 <b-button :variant="secondary" to="/funds">FUNDS</b-button>
+                 <b-button :variant="secondary" to="/incomes">INCOMES</b-button>
+                 <b-button :variant="secondary" to="/spendings">SPENDINGS</b-button>
+                 <b-button :variant="secondary" to="/groups">GROUPS</b-button>
+            </b-button-group>
+            </div>
+      </aside>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SideBar"
+    }
+</script>
+
+<style scoped>
+aside{
+  position:fixed;
+  background: orange;
+  float:left;
+  width: 250px;
+  height: 100vh;
+}
+#navbtns{
+  width: 140px;
+  margin-left: 55px;
+  position: absolute;
+  bottom: 275.2px;
+  margin-bottom: 20px;
+}
+</style>
---
+++ iBudget/static/src/pages/Groups.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="content">
+      <div class="text">
+      <h1>There will be groups...</h1>
+    </div>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Groups"
+    }
+</script>
+
+<style scoped>
+.content{
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+}
+  .text{
+    width: fit-content;
+    margin:  auto;
+  }
+</style>
