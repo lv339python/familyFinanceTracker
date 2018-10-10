d3c4dbe2af17d0668471622b4c121329f83ab1cc
Rocckk <igorrrock.it@gmail.com>
2018-10-10 15:22:25 +0300

    validators added

---
+++ iBudget/authentication/migrations/0004_userprofile_is_sys_admin.py
@@ -0,0 +1,4 @@
+Unstaged changes after reset:
+M	.gitignore
+D	iBudget/authentication/migrations/0003_userprofile_is_sys_admin.py
+M	iBudget/authentication/migrations/0004_userprofile_is_sys_admin.py
