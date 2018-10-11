7ef82a6d5683fc6effaa1d80f6027a3ba5b4920d
Rocckk <igorrrock.it@gmail.com>
2018-10-11 12:53:53 +0300

    implemented group limits

--- iBudget/static/src/components/index.js
+++ iBudget/static/src/components/index.js
@@ -2,6 +2,8 @@ import Login from './Login';
 import Spend from './Spend';
 import SideBar from './SideBar';
 import Spending_registration from './Spending_registration';
+import Limit from './Limit';
 
-export {Login, SideBar,Spending_registration, Spend};
+export {Login, SideBar, Spending_registration, Spend, Limit};
+
 
