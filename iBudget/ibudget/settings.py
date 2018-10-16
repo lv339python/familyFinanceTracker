83acc9be2f27731ae3a332b73e76057c9c77a2b6
Alex <abysho666@gmail.com>
2018-10-16 13:20:18 +0300

    Group balance

--- iBudget/ibudget/settings.py
+++ iBudget/ibudget/settings.py
@@ -43,13 +43,11 @@ INSTALLED_APPS = [
     'spending',
     'spending_history',
     'webpack_loader',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
