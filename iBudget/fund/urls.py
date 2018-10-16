e74b51d40a047490ebd48a7b81a099525f983965
Sent1nelDnoir <sent1neld.noir@gmail.com>
2018-10-16 23:24:35 +0300

    goal

--- iBudget/fund/urls.py
+++ iBudget/fund/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include, re_path
-from .views import show_fund,show_goal_data
+from .views import show_fund, show_goal_data
+
 urlpatterns = [
     re_path(r'^$', show_fund),
-    path('show_goal_data/', show_goal_data)
+    path('show_goal_data/', show_goal_data),
+
 ]
--- iBudget/fund/views.py
+++ iBudget/fund/views.py
@@ -3,9 +3,9 @@ This module provides functions for handling fund view.
 """
 import json
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_http_methods
-from .models import FundCategories
+from .models import FundCategories, FinancialGoal
 from income_history.models import IncomeHistory
 
 
@@ -26,13 +26,36 @@ def show_fund(request):
         return JsonResponse(user_funds, status=200, safe=False)
     return JsonResponse({}, status=400)
 
+
+def list_goal_user(user):
+    user = 1
+    list_fund =[]
+    for entry in FundCategories.filter_by_user(user):
+        list_fund.append(entry.id)
+    list_goal = []
+    for entry in list_fund:
+        financial_goal=FinancialGoal.objects.filter(fund=entry)
+        if financial_goal:
+            list_goal.append(entry)
+    return list_goal
+
+
+
+
 @require_http_methods(["GET"])
 def show_goal_data(request):
     user = request.user
+    user = 1
     if user:
         user_goal_statistic = []
-        for entry in FundCategories.filter_by_user(user):
-            for item in IncomeHistory.filter_by_fund_id(entry.id):
-                user_goal_statistic.append({'date':item.date, "value":item.value})
-        return JsonResponse(user_goal_statistic, status=200, safe=False)
+        for entry in list_goal_user(user):
+            fund_category= FundCategories.get_by_id(entry)
+            list_transactions = []
+            for item in IncomeHistory.objects.filter(fund=entry, date__range=[fund_category.goal.start_date,fund_category.goal.finish_date]):
+                list_transactions.append(item.value)
+            user_goal_statistic.append({"id": entry, "name": fund_category.name,"value": fund_category.goal.value,"start_date":fund_category.goal.start_date,"finish_date":fund_category.goal.finish_date,
+                                        'transaction': list_transactions})
+        return HttpResponse(user_goal_statistic, status=200)
     return JsonResponse({}, status=400)
+
+
