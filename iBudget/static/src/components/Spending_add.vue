f1be9f636f826097af49fa76e7af777e5ff02ec5
HalynaDom <halyna.domanska@gmail.com>
2018-10-17 12:35:51 +0300

    Indentation fix

--- iBudget/static/src/components/Spending_add.vue
+++ iBudget/static/src/components/Spending_add.vue
@@ -2,21 +2,21 @@
     <div id="spending_add">
 
         <div class="col-md-4">
-          <hr>
-          <div class="form-group">
-            <label for="name">Name of spending:</label>
-            <input v-model="newName" placeholder="Name" id="name">
-           </div>
+            <hr>
+            <div class="form-group">
+                <label for="name">Name of spending:</label>
+                <input v-model="newName" placeholder="Name" id="name">
+            </div>
         </div>
 
         <div class="col-md-4">
             <icon_getter @get_name='onGet_name'> </icon_getter>
         </div>
 
-        <div  v-show="isValidData">
-           <hr>
-           <button v-on:click="createSpending" :variant="secondary" >Create spending</button>
-           <button v-on:click="createDone" :variant="secondary" v-show="isDone">{{msg}}</button>
+        <div v-show="isValidData">
+            <hr>
+            <button v-on:click="createSpending" :variant="secondary" >Create spending</button>
+            <button v-on:click="createDone" :variant="secondary" v-show="isDone">{{msg}}</button>
         </div>
 
     </div>
@@ -58,13 +58,11 @@ export default {
                 method: 'post',
                 url: '/api/v1/spending/add/',
                 data: {
-                'name': this.newName,
-                'icon': this.selectedIcon
+                    'name': this.newName,
+                    'icon': this.selectedIcon
                 }
             }).then(response =>(this.msg = response.data));
-        this.isDone=true;
-        console.log(this.newName,
-                this.selectedIcon)
+            this.isDone=true;
         },
         createDone: function (event) {
             this.$router.go('/spending_add')
