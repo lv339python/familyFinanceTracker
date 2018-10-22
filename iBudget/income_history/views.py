7e6604d815ed89d9a4f8f57c9e1554831092b4e8
Rocckk <igorrrock.it@gmail.com>
2018-10-22 20:21:15 +0300

    more info

--- iBudget/income_history/views.py
+++ iBudget/income_history/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_http_methods
 import json
-from .models import IncomeHistory, IncomeCategories
+from .models import IncomeCategories, FundCategories, IncomeHistory
 
 
 @require_http_methods(['POST'])
@@ -9,12 +9,26 @@ def test_track(request):
     content = request.body
     content = json.loads(content)
     user_id = request.user
-    # income_inst = IncomeCategories.objects.get(owner_id=user['id'])
-    # print(income_inst)
     if len(content) <= 1:
         return HttpResponse('You did not choose any dates or you chose only one date out of two',
                             status=400)
-    incomes_by_dates = IncomeHistory.objects.filter(income_id__incomecategories__owner_id=user_id)
-    # date__range=(content['start'], content['end'])
-    print(incomes_by_dates)
+    incomes_funds = IncomeHistory.objects.filter(date__date__range=(content['start'], content['end']))
+    l = [{i.income_id: i.fund_id for i in incomes_funds}]
+    print(l)
+    # incomes_by_dates = IncomeCategories.objects.filter(owner_id=user_id,
+    #                                                    income_history__date__range=(
+    #                                                    content['start'], content['end']))
+    # list_of_incomes_by_dates = []
+    # list_of_funds_by_dates = []
+    # for income in incomes_by_dates:
+    #     list_of_incomes_by_dates.append(income.id)
+    # print(list_of_incomes_by_dates)
+    #
+    # funds_by_dates = FundCategories.objects.filter(owner_id=user_id,
+    #                                                income_history__income_id__in=
+    #                                                list_of_incomes_by_dates)
+    # for fund in funds_by_dates:
+    #     list_of_funds_by_dates.append(fund.name)
+    # print(list_of_funds_by_dates)
+
     return JsonResponse(content, status=200)
