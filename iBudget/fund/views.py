73492eafdabe9fe838545c60fae7c4d8bb7fff54
ValentynLiashenko <valentynoxk@gmail.com>
2018-10-24 01:14:05 +0300

    update validation

--- iBudget/fund/views.py
+++ iBudget/fund/views.py
@@ -9,7 +9,7 @@ from django.views.decorators.http import require_http_methods
 from group.models import Group, SharedFunds
 from utils.validators import input_fund_registration_validate, date_range_validate, is_valid_data_create_new_fund
 from utils.transaction import save_new_fund
-from utils.get_role import is_user_member_group
+from utils.get_role import is_user_admin_group
 from .models import FundCategories, FinancialGoal
 
 
@@ -108,7 +108,7 @@ def create_new_fund(request):
         group = Group.get_group_by_id(shared_group)
         if not group:
             return HttpResponse(status=400)
-        if not is_user_member_group(user=user, group_id=group.id):
+        if not is_user_admin_group(group.id, user):
             return HttpResponse(status=406)
     if save_new_fund(name=data["name"], icon=data["icon"], is_shared=is_shared, owner=user, shared_group=shared_group):
         return HttpResponse(status=201)
