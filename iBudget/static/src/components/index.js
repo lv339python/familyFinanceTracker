d6779ea76d96439203fc479c0506f6952057efc9
HalynaDom <halyna.domanska@gmail.com>
2018-10-23 14:38:16 +0300

    Representation of spending history

--- iBudget/static/src/components/index.js
+++ iBudget/static/src/components/index.js
@@ -7,8 +7,10 @@ import Icon_getter from './Icon_getter';
 import Spending_add from './Spending_add';
 import Financial_goal from './Financial_goal';
 import Spending_history from './Spending_history';
+import Funds_registration from './Funds_registration';
 
-export {Login, SideBar, Spending_registration, Spend, Limit, Icon_getter, Spending_add,  Financial_goal, Funds_registration, Spending_history};
+export {Login, SideBar, Spending_registration, Spend, Limit, Icon_getter, Spending_add,
+    Financial_goal, Funds_registration, Spending_history};
 
 
 
