e5f60737686d8e64ca1239f6cacb609ec634a9bf
Rocckk <igorrrock.it@gmail.com>
2018-10-23 10:24:43 +0300

    more updates

--- iBudget/income_history/views.py
+++ iBudget/income_history/views.py
@@ -12,9 +12,28 @@ def test_track(request):
     if len(content) <= 1:
         return HttpResponse('You did not choose any dates or you chose only one date out of two',
                             status=400)
-    incomes_funds = IncomeHistory.objects.filter(date__date__range=(content['start'], content['end']))
-    l = [{i.income_id: i.fund_id for i in incomes_funds}]
-    print(l)
+    incomes_funds = IncomeHistory.objects.filter(date__date__range=(content['start'], content['end']), income_id__owner_id=user_id)
+    print(incomes_funds)
+    # dict_of_incomes_funds = [{'income': i.income_id, 'fund': i.fund_id} for i in incomes_funds]
+    dict_of_incomes_funds = []
+    for i in incomes_funds:
+        print(i.date)
+    print(dict_of_incomes_funds)
+    list_of_incomes_funds = []
+    for n in range(len(dict_of_incomes_funds)):
+        list_of_incomes_funds.append({IncomeCategories.objects.get(id=dict_of_incomes_funds[n]['income']).name :
+                      FundCategories.objects.get(id=dict_of_incomes_funds[n]['fund']).name})
+
+    print(list_of_incomes_funds)
+
+
+
+    # for l in dict_of_incomes_funds:
+    #     for k,v in l.items():
+    #         k = income_obj
+    #         v = fund_obj
+    #
+    # print (dict_of_incomes_funds)
     # incomes_by_dates = IncomeCategories.objects.filter(owner_id=user_id,
     #                                                    income_history__date__range=(
     #                                                    content['start'], content['end']))
@@ -31,4 +50,4 @@ def test_track(request):
     #     list_of_funds_by_dates.append(fund.name)
     # print(list_of_funds_by_dates)
 
-    return JsonResponse(content, status=200)
+    return JsonResponse(list_of_incomes_funds, safe=False, status=200)
