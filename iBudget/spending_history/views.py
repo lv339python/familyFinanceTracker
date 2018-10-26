6f88341a596f96fe76308bca89c0c6b1446c2f86
ValentynLiashenko <valentynoxk@gmail.com>
2018-10-26 09:08:41 +0300

    spending registration validate update

--- iBudget/spending_history/views.py
+++ iBudget/spending_history/views.py
@@ -36,6 +36,14 @@ def register_spending(request):
     fund = FundCategories.get_by_id(int(data["type_of_pay"]))
     if not fund:
         return HttpResponse(status=400)
+
+    if spending.is_shared:
+        if not SharedSpendingCategories.objects.get(spending_categories=spending):
+            return HttpResponse(status=403)
+    else:
+        if not spending.owner == user:
+            return HttpResponse(status=403)
+
     value = Decimal(data["value"])
     comment = data["comment"]
 
