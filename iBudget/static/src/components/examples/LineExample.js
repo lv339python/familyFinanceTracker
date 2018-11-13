a0dd76f279a613a026042bd4d7e3995921d4515c
Rocckk <igorrrock.it@gmail.com>
2018-11-13 15:02:47 +0200

    small changes for table with income tracker

--- iBudget/static/src/components/examples/LineExample.js
+++ iBudget/static/src/components/examples/LineExample.js
@@ -25,31 +25,16 @@ export default {
               datas.datasets[dict].borderColor = this.color[dict];
               datas.datasets[dict].data = [];
               for (var val in value[0]){
-                  //console.log("%%", value[0][val]);
                   datas.datasets[dict].data.push({x:value[0][val]});
               }
           }
           for (var dict in this.amounts){
-              //var key = Object.keys(this.amounts[dict]);
               var value = Object.values(this.amounts[dict]);
-              //datas.datasets.push({label:key});
-              //datas.datasets[dict].fill = false;
-              //datas.datasets[dict].borderColor = this.color[dict];
-              //datas.datasets[dict].data = [];
               for (var val in value[0]){
-                  console.log("%%", datas.datasets[dict].data[val]);
                   datas.datasets[dict].data[val].y = value[0][val];
               }
           }
-          console.log('##',datas);
           this.datasets = datas
-              // {labels:this.x_axis, datasets:[{
-              // label:'asa',
-              // data: [{x:"2018-10-01", y:5}, {x:"2018-10-05", y:10}, {x:"2018-10-20", y: 15}]},
-              // {
-              // label:'bdb',
-              // data: [{x:"2018-10-01", y:5}, {x:"2018-10-04", y:23}]}
-              // ]}
         }
     },
   mounted (){
